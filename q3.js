c1e8f7a425f7068034f5f580f164ff7be966065e
alonosika <alonosika@gmail.com>
2021-01-20 16:20:44 +0200

    q3 answer

---
+++ q3.js
@@ -0,0 +1,20 @@
+  function run() {
+    const input = document.getElementById("input").value;
+    const input1 = document.getElementById("input1").value;
+    const output = document.getElementById("output");  
+    let out = "";
+    out=  arrFirst(eval(input) , input1);
+    output.innerText = out;
+/**/
+    // write your code here
+
+    function arrFirst(array, n){
+        if (n > 0){
+            return array.slice(0, n);
+        } else{
+            return ("[]" + array[0]);
+        } 
+
+/**/
+      }
+  }
